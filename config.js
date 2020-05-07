26ebfb70e846aa778532a4abb2f7f4d50db65b58
Luc Hoang <luc.hn@teko.vn>
2020-05-07 21:40:52 +0700

    Initial commit from gatsby: (https://github.com/luc-hn/gatsby-starter.git)

---
+++ config.js
@@ -0,0 +1,50 @@
+"use strict";
+
+module.exports = {
+  url: "https://lumen.netlify.com",
+  pathPrefix: "/",
+  title: "Tiếng Trung Móng Cái",
+  subtitle:
+    "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
+  postsPerPage: 4,
+  googleAnalyticsId: "UA-73379983-2",
+  useKatex: false,
+  menu: [
+    {
+      label: "Trang chủ",
+      path: "/",
+    },
+    {
+      label: "Giới thiệu",
+      path: "/pages/about",
+    },
+    {
+      label: "Liên hệ",
+      path: "/pages/contacts",
+    },
+  ],
+  author: {
+    name: "Tiếng Trung Móng Cái",
+    photo: "/hoalinhhsk.jpg",
+    bio: "Chuyên đào tạo tiếng Trung giao tiếp",
+    contacts: {
+      email: "",
+      facebook: "hoalinhhskmc",
+      telegram: "",
+      twitter: "",
+      github: "",
+      rss: "",
+      vkontakte: "",
+      linkedin: "",
+      instagram: "",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
+};
