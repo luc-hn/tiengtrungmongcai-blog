4d3abfb206562326082ce470894fe7a1a33e8308
Luc Hoang <luc.hn@teko.vn>
2020-05-31 18:14:49 +0700

    udpate text

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,24 +1,24 @@
 // @flow strict
-import React from 'react';
-import { getContactHref } from '../../../utils';
-import styles from './Author.module.scss';
-import { useSiteMetadata } from '../../../hooks';
+import React from "react";
+import { getContactHref } from "../../../utils";
+import styles from "./Author.module.scss";
+import { useSiteMetadata } from "../../../hooks";
 
 const Author = () => {
   const { author } = useSiteMetadata();
 
   return (
-    <div className={styles['author']}>
-      <p className={styles['author__bio']}>
+    <div className={styles["author"]}>
+      <p className={styles["author__bio"]}>
         {author.bio}
-        <a
+        {/* <a
           className={styles['author__bio-twitter']}
           href={getContactHref('twitter', author.contacts.twitter)}
           rel="noopener noreferrer"
           target="_blank"
         >
           <strong>{author.name}</strong> on Twitter
-        </a>
+        </a> */}
       </p>
     </div>
   );
